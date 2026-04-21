d9a22d6d2baf42c2ed1cba4efcbd6fd4528e1f71
Brian Cefali <brocef@users.noreply.github.com>
2026-04-20 23:53:39 -0400

    refactor(api-client): convert user/sources.ts to *Impl form (1 method)

--- src/api-client/user/sources.ts
+++ src/api-client/user/sources.ts
@@ -1,14 +1,14 @@
 import { UserSourcesResponseSchema } from "../../schemas/api/sources.js"
 import { parseResponse } from "../../utils/utils.js"
+import type { TApiClientConfig } from "../config.js"
+import { resolveBaseUrl } from "../internal.js"
 
-export async function getUserSources(
-    fetchFn: typeof fetch = fetch,
-    urlPrefix = ""
-) {
-    const url = new URL(`${urlPrefix}/api/v1/user/sources`, "http://localhost")
+export async function getUserSourcesImpl(config: TApiClientConfig) {
+    const baseUrl = resolveBaseUrl(config)
+    const url = new URL(`${baseUrl}/api/v1/user/sources`, "http://localhost")
 
     return await parseResponse(
-        await fetchFn(url.pathname + url.search, { method: "GET" }),
-        UserSourcesResponseSchema
+        await config.fetchImpl(url.pathname + url.search, { method: "GET" }),
+        UserSourcesResponseSchema,
     )
 }
